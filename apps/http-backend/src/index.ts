bd8d0eafc1d44682032def8c61e9f9006fa84b2a
Nitinxtech <nitinp3006@gmail.com>
2025-05-19 18:48:08 +0530

    Done with basic Http-backend and ws Backend

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,59 @@
+ import express from "express";
+ const app = express();
+
+ import jwt from 'jsonwebtoken';
+ import { JWT_SECRET } from "@repo/backend-common/config";
+ import { middleware } from "./middleware";
+ import { CreateRoomSchema, CreateUserSchema, SigninSchema } from "@repo/common/types";
+
+ app.post('/signup', (req, res) => {
+
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message: "Incorrect Inputs"
+        })
+        return;
+    }
+
+    res.json({
+        userId: "123"
+    })
+ })
+
+ app.post('/signin', (req, res) => {
+
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message: "Incorrect Inputs"
+        })
+        return;
+    }
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
+
+    res.json({
+        token
+    })
+ })
+
+ app.post('/room', middleware, (req, res) => {
+
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message: "Incorrect Inputs"
+        })
+        return;
+    }
+
+    res.json({
+        roomId: 123
+    })
+ })
+
+ app.listen(3001);
